2ddcab4b9c00e13e0504a415bdd52c37182a1d28
Домрачев Александр <sasha_domrachev@mail.ru>
2022-11-10 18:05:07 +0200

    Add Explorer

---
+++ TreeSize.Handler/DirectoryContent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace TreeSize.Handler
+{
+    public class DirectoryContent
+    {
+        public BitmapImage Icon { get; set; }
+        public string Name { get; set; }
+        public long? TotalSize { get; set; }
+        public long? FreeSpace { get; set; }
+        public DateTime? LastWriteTime { get; set; }
+    }
+}
--- TreeSizeApp/MainWindow.xaml.cs
+++ TreeSizeApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TreeSize.Handler;
 
 namespace TreeSizeApp
 {
@@ -20,9 +21,36 @@ namespace TreeSizeApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FormRender _formRender;
         public MainWindow()
         {
             InitializeComponent();
+            _formRender = new FormRender(ListView, TreeView, TextBox, this);
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (ListView.SelectedItem != null)
+            {
+                if (TextBox.Text == String.Empty)
+                {
+                    _formRender.GoTo(((DirectoryContent)ListView.SelectedItem).Name);
+                    TextBox.Text = _formRender.PathToFile;
+                }
+                else
+                {
+                    _formRender.GoToRelative(((DirectoryContent)ListView.SelectedItem).Name);
+                    TextBox.Text = _formRender.PathToFile;
+                }
+            }
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                _formRender.GoTo(TextBox.Text);
+            }
         }
     }
 }
