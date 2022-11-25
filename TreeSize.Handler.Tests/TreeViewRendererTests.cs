9d3a7bfa4e00ae557be80b5889b56805851509eb
Домрачев Александр <sasha_domrachev@mail.ru>
2022-11-25 15:07:41 +0200

    Edit

--- TreeSize.Handler.Tests/TreeViewRendererTests.cs
+++ TreeSize.Handler.Tests/TreeViewRendererTests.cs
@@ -1,6 +1,7 @@
 using Moq;
 using System.Collections.ObjectModel;
 using System.Runtime.Serialization;
+using System.Windows.Controls;
 using System.Windows.Threading;
 using TreeSize.Handler.Interfaces;
 using TreeSize.Handler.Nodes;
@@ -11,42 +12,21 @@ namespace TreeSize.Handler.Tests
     [TestClass]
     public class TreeViewRendererTests
     {
-        DirectoryManager directoryManager = new DirectoryManager();
-        List<DriveInfo> allDrive = new List<DriveInfo>();
-        FileManager fileManager = new FileManager();
-        MainThreadDispatcher mainThreadDispatcher = new MainThreadDispatcher();
-
-        public TreeViewRendererTests()
-        {
-            foreach (var driveInfo in DriveInfo.GetDrives())
-            {
-                if (driveInfo.IsReady)
-                {
-                    allDrive.Add(driveInfo);
-                }
-            }
-        }
 
         [TestMethod]
-        public async Task TestMethod1()
+        public async Task CountDirectoryToTheNode()
         {
             Mock<IFileManager> mock = new Mock<IFileManager>();
             Mock<IMainThreadDispatcher> dispetcherMock = new Mock<IMainThreadDispatcher>();
-            directoryManager.Create(allDrive[0].ToString(), "Test1");
-            directoryManager.Create(allDrive[0].ToString(), "Test1/sub1");
-            directoryManager.Create(allDrive[0].ToString(), "Test1/sub1/sub2");
-            directoryManager.Create(allDrive[0].ToString(), "Test1/sub1/sub2/sub3");
-            fileManager.Create(allDrive[0].ToString(), "Test1/sub1/sub2/sub3/testFile1.txt");
-            fileManager.Create(allDrive[0].ToString(), "Test1/sub1/sub2/sub3/testFile2.txt");
-            DirectoryInfo dir = new DirectoryInfo(allDrive[0].ToString() + "Test1");
+            DirectoryInfo dir = new DirectoryInfo("Test1");
+            dir.CreateSubdirectory("sub1").CreateSubdirectory("sub2").CreateSubdirectory("sub2");
             mock.Setup(m => m.GetDrives()).Returns(new DirectoryInfo[]{
                 dir
             });
-            TreeViewRenderer treeViewRenderer = new TreeViewRenderer(mock.Object, mainThreadDispatcher);
+            dispetcherMock.Setup(m => m.DispatchAsync(It.IsAny<Action>()));
+            TreeViewRenderer treeViewRenderer = new TreeViewRenderer(mock.Object, dispetcherMock.Object);
             ObservableCollection<Node> nodes = await treeViewRenderer.RefreshNodes();
-            directoryManager.Delete(allDrive[0].ToString() + "Test1");
             Assert.IsTrue(nodes[0].CountFoldersAndBytesAndFiles.Folders == 3);
-            Assert.IsTrue(nodes[0].CountFoldersAndBytesAndFiles.Files == 2);
         }
     }
 }
